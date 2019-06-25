b9cfaf04dbe173d005bad66e0a2e36868f2122ec
Jackmc <jackmc1122@gmail.com>
2019-06-25 16:02:33 -0700

    Add index files and outline initial steps.

---
+++ index.js
@@ -0,0 +1,8 @@
+/* 
+1. Add files to test with
+2. Get all the files
+  a. Read its content
+  b. Read its date last edited
+3. loop through all of the files
+4. Rename each file based on content and edited data
+*/
